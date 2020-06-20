19459c5d9244244d6a0a942d6b66b6ea1c261ef4
Tyler Sharp <johntylersharp@gmail.com>
2020-06-19 20:30:50 -0400

    final endpoint and build

---
+++ petful-client-template/build/precache-manifest.f15fc9ca7088f8f4ae1791546ee025eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2245775b973b70d7a7cffc670013ec0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3db310f80471ec79c79b",
+    "url": "/static/css/main.e1c3183b.chunk.css"
+  },
+  {
+    "revision": "871a8457ef62e1119bd7",
+    "url": "/static/js/2.9228e00d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.9228e00d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db310f80471ec79c79b",
+    "url": "/static/js/main.4277f384.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
